c08e5c1fa4a2d132d5ca68b5fffd31f398c6d776
Neil-Kasson <Neil-Kasson@users.noreply.github.com>
2020-10-10 14:27:55 -0400

    added CharacterArray

---
+++ javaControlFlow/test/src/CharacterArray.java
@@ -0,0 +1,11 @@
+public class CharacterArray {
+	public static void main(String[] args) {
+		String s = "Random String";
+
+		char [] c = s.toCharArray();
+
+		for (int i = 0; i < c.length; i++) {
+			System.out.println(c[i]);
+		}
+	}
+}
